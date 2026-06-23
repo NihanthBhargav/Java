e9ce939808dba497435089b2d9f5d2e10eb59114
NihanthBhargav <nihanthbhargav@gmail.com>
2026-06-23 19:31:08 +0530

    Added new files

---
+++ Arrays/removeDuplicates.java
@@ -0,0 +1,29 @@
+package Arrays;
+
+public class removeDuplicates{
+    public static int arr(int[] arr){
+        if(arr.length < 0){
+            return 0;
+        }
+
+        int i = 0;
+
+        for (int j = 1; j < arr.length; j++){
+            if(arr[j] != arr[i]){
+                i++;
+                arr[i] = arr[j];
+            }
+        }
+
+        return i + 1;
+    }
+    public static void main(String[] args){
+        int[] arr = {1,1,2,2,3,4,5,5};
+        
+        int k = arr(arr);
+        for (int i = 0; i < k; i++){
+            System.out.print(arr[i]+ " ");
+        }
+
+    }
+}
